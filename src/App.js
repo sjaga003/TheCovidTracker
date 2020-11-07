e3e2058b88cb10d768989c0ba5047dba32014c41
Suhas Jagannath <sjaga003@ucr.edu>
2020-11-07 15:51:44 -0800

    Add date picker

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import StatsBox from './components/stats-box.js';
+import DatePicker from 'react-datepicker';
+import Moment from 'react-moment';
+
+import 'react-datepicker/dist/react-datepicker.css';
 
 function App() {
-  const [cases, setCases] = useState(0);
-  const [deaths, setDeaths] = useState(0);
-  const [tests, setTests] = useState(0);
   const [covidData, setCovidData] = useState({});
+  const [startDate, setStartDate] = useState(() => {
+    let today = new Date();
+    return today.setDate(today.getDate() - 1);
+  });
+  const [calendarVisible, setCalendarVisible] = useState(false);
 
   const fetchFromApi = async () => {
     const data = await fetch('https://api.covidtracking.com/v1/us/daily.json');
@@ -25,11 +31,11 @@ function App() {
       // const selectedDate = new Date(completeDate);
       // console.log(selectedDate);
       console.log(jsonData);
-      const date = new Date();
-      const dateString = `${date.getFullYear()}${(
+      const dateVal = new Date(startDate);
+      const dateString = `${dateVal.getFullYear()}${(
         '0' +
-        (date.getMonth() + 1)
-      ).slice(-2)}${('0' + date.getDay()).slice(-2)}`;
+        (dateVal.getMonth() + 1)
+      ).slice(-2)}${('0' + dateVal.getDay()).slice(-2)}`;
 
       jsonData.map((data) => {
         if (data.date == dateString) {
@@ -46,9 +52,32 @@ function App() {
     });
   };
 
+  const isValidDate = (date) => {
+    let today = new Date();
+    if (
+      date > new Date('03/07/2020') &&
+      date < today.setDate(today.getDate() - 1)
+    ) {
+      return date;
+    }
+  };
+
+  const clickDate = () => {
+    if (calendarVisible === 'none') {
+      console.log(calendarVisible);
+      setCalendarVisible('block');
+    } else {
+      setCalendarVisible('none');
+    }
+  };
+
   useEffect(() => {
     getData();
   }, []);
+
+  useEffect(() => {
+    getData();
+  }, [startDate]);
 
   return (
     <div className="App">
@@ -58,15 +87,30 @@ function App() {
           <div className="header-data">
             <h3>United States</h3>
             <div className="header-date">
-              As of November 11, 2020{' '}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-              >
-                <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z" />
-              </svg>
+              <div className="calendar" style={{ display: calendarVisible }}>
+                {' '}
+                <DatePicker
+                  selected={startDate}
+                  onChange={(date) => {
+                    setStartDate(date);
+                    clickDate();
+                  }}
+                  filterDate={isValidDate}
+                  inline
+                />
+              </div>
+              As of{' '}
+              <span onClick={clickDate} className="date-moment">
+                <Moment date={startDate} format="MMMM Do, YYYY " />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M20 20h-4v-4h4v4zm-6-10h-4v4h4v-4zm6 0h-4v4h4v-4zm-12 6h-4v4h4v-4zm6 0h-4v4h4v-4zm-6-6h-4v4h4v-4zm16-8v22h-24v-22h3v1c0 1.103.897 2 2 2s2-.897 2-2v-1h10v1c0 1.103.897 2 2 2s2-.897 2-2v-1h3zm-2 6h-20v14h20v-14zm-2-7c0-.552-.447-1-1-1s-1 .448-1 1v2c0 .552.447 1 1 1s1-.448 1-1v-2zm-14 2c0 .552-.447 1-1 1s-1-.448-1-1v-2c0-.552.447-1 1-1s1 .448 1 1v2z" />
+                </svg>
+              </span>
             </div>
             <div className="header-stats">
               <StatsBox
