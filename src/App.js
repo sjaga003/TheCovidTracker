f95d652cfd29c496b592bca12d3d22a252c8e8d0
Suhas Jagannath <sjaga003@ucr.edu>
2020-11-07 08:38:36 -0800

    add fetch data from covid api

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const fetchFromApi = async () => {
+    const data = await fetch('https://api.covidtracking.com/v1/us/daily.json');
+    const jsonData = data.json();
+    return jsonData;
+  };
+
+  console.log(fetchFromApi());
+
   return (
     <div className="App">
       <header className="App-header"></header>
       <main>
         <section>
-          <div>HELLO</div>
+          <div></div>
         </section>
       </main>
     </div>
